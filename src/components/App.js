279a4a7f26d2550d48ec197b952bece311719d2f
David Whitney <codifyamplify@gmail.com>
2022-11-04 16:27:32 -0400

    commit tree 5c25e40c1be2a027d3aa6b17db84c4652fe5cc2e

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, {useEffect, useState} from "react"
+
+function App(){
+    // const [isLoaded, setIsLoaded] = useState(false)
+    console.log("beginning of app")
+    const [dogPic, setDogPic] = useState(null)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data.message)
+                console.log(data.status)
+                setDogPic(data.message)
+            })
+
+    }, [])
+    if (!dogPic) return <p>Loading...</p>
+    return <img alt="A Random Dog" src={dogPic}/>
+}
+
+export default App
